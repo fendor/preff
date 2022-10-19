2afd263c42cb09997be69e073449c097b31cfc3e
Fendor <power.walross@gmail.com>
2022-10-19 11:54:02 +0200

    Add simple State example

--- src/Simple/State.hs
+++ src/Simple/State.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE TypeApplications #-}
-
 {-# LANGUAGE RebindableSyntax #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Simple.State where
 
@@ -21,6 +20,9 @@ getS = Impure GetS return
 putS :: s -> IProg (StateS s) g p q ()
 putS s = Impure (PutS s) return
 
+localS :: (s -> s) -> IProg f (StateG s) q r a -> IProg f (StateG s) p r a
+localS f prog = Scope (LocalG f) prog return
+
 runState :: s -> IProg (StateS s) (StateG s) p q a -> (s, a)
 runState s (Pure a) = (s, a)
 runState s (Impure GetS k) = runState s (k s)
@@ -29,8 +31,19 @@ runState s (Scope (LocalG f) prog k) = runState s (k x)
   where
     (_, x) = runState (f s) prog
 
-stateExample :: IProg (StateS Int) (StateG Int) () () String
+stateExample :: IProg (StateS Int) (StateG Int) p q String
 stateExample = do
-    i <- getS @Int
-    putS (i+i)
-    Utils.return $ show i
+  i <- getS
+  putS (i + i)
+  return $ show i
+
+stateWithLocal :: IProg (StateS Int) (StateG Int) p q String
+stateWithLocal = do
+  n <- getS
+  x <- localS (+ n) stateExample
+  return $ x ++ ", initial: " ++ show n
+
+ambiguityExample :: IProg (StateS Int) a p q ()
+ambiguityExample = do
+  _i <- getS
+  return ()
