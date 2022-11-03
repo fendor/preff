bde2632d0acf532a01dd987a37bce32d01b0aac0
Fendor <power.walross@gmail.com>
2022-11-03 14:37:38 +0100

    Write State effect that may change its type value

--- src/Parameterised/State.hs
+++ src/Parameterised/State.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE QualifiedDo #-}
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
 
 module Parameterised.State where
 
 import Data.Kind
 import Utils
+import qualified Utils as Ix
 import Prelude hiding (Monad (..))
 
 data StateF p q x where
@@ -37,3 +39,28 @@ get a = Impure (Get a) return
 fork s = Scope Fork s return
 
 finish s = Scope Finish s return
+
+data StateA p q a where
+  PutA :: x -> StateA p x ()
+  GetA :: StateA p p p
+
+putA a = Op (Inl $ PutA a) (IKleisliTupled return)
+
+getA = Op (Inl GetA) (IKleisliTupled return)
+
+stateCExp :: Sem (StateA :+: eff) '(String, ()) '(Int, ()) ()
+stateCExp = Ix.do
+  s <- getA
+  putA (read s + 100)
+
+runStateA ::
+  p ->
+  Sem (StateA :+: eff) '(p, sr1) '(q, sr2) a ->
+  Sem eff sr1 sr2 (a, q)
+runStateA p (Value x) = Ix.return (x, p)
+runStateA p (Op (Inl GetA) k) =
+  runStateA p (runIKleisliTupled k p)
+runStateA _ (Op (Inl (PutA q)) k) =
+  runStateA q (runIKleisliTupled k ())
+runStateA p (Op (Inr cmd) k) =
+  Op cmd $ IKleisliTupled $ \x -> runStateA p (runIKleisliTupled k x)
