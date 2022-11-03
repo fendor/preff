10831f5298cca6343026e2a25a8f88d2ff528a1a
Fendor <power.walross@gmail.com>
2022-11-03 14:37:21 +0100

    Write basic runner for Protocol definitions

--- src/Parameterised/Protocol.hs
+++ src/Parameterised/Protocol.hs
@@ -5,20 +5,21 @@ import Data.Kind
 import Utils
 import qualified Utils as Ix
 
-type End :: Type
+-- type End :: Type
 data End
 
-type (:!) :: forall k . Type -> k -> Type
+-- type (:!) :: forall k . Type -> k -> Type
 data (:!) a r
+infixr 5 :!
 
-infixr 5 :?
-type (:?) :: forall k . Type -> k -> Type
+-- type (:?) :: forall k . Type -> k -> Type
 data (:?) a r
+infixr 5 :?
 
-type (:|) :: forall k . k -> k -> Type
+-- type (:|) :: forall k . k -> k -> Type
 data (:|) sl sr
 
-type (:&) :: forall k . k -> k -> Type
+-- type (:&) :: forall k . k -> k -> Type
 data (:&) sl sr
 
 data Protocol p q r where
@@ -26,12 +27,13 @@ data Protocol p q r where
     Recv :: Protocol (a :? p) p a
     Sel1 :: Protocol (a :| b) a ()
     Sel2 :: Protocol (a :| b) b ()
+    Offer :: Protocol a p () -> Protocol b p () -> Protocol (a :& b) p ()
 
 type family Dual proc where
   Dual (a :! p) = a :? Dual p
   Dual (a :? p) = a :! Dual p
-  Dual (a :| b) = Dual a :& Dual b
-  Dual (a :& b) = Dual a :| Dual a
+  -- Dual (a :| b) = Dual a :& Dual b
+  -- Dual (a :& b) = Dual a :| Dual a
   Dual End = End
 
 send :: a -> Sem (Protocol :+: IIdentity) '(a :! p, sr) '(p, sr) ()
@@ -47,16 +49,26 @@ sel2 :: Sem (Protocol :+: IIdentity) '(a :| b, sr) '(b, sr) ()
 sel2 = Op (Inl Sel2)  (IKleisliTupled Utils.return)
 
 -- simpleProtocol :: Sem (Protocol :+: IIdentity) '(p, sr) '(Int :! String :? p, sr) String
-simpleProtocol :: forall {k} {p} {sr :: k}.
+simpleServer ::
   Sem
     (Protocol :+: IIdentity)
     '(Int :! (String :? p), sr)
     '(p, sr)
     String
-simpleProtocol = Ix.do
+simpleServer = Ix.do
   send @Int 5
   s <- recv @String
   Ix.return s
+
+simpleClient ::
+  Sem
+    (Protocol :+: IIdentity)
+    '(Int :? (String :! p), sr)
+    '(p, sr)
+    ()
+simpleClient = Ix.do
+  n <- recv @Int
+  send (show $ n * 25)
 
 choice = Ix.do
   send @Int 5
@@ -81,8 +93,33 @@ choice2 = Ix.do
       Ix.return x
     )
 
-connect :: Dual p ~ q =>
-  Sem (Protocol :+: eff) '(q, sr) '(End, sr) a ->
-  Sem (Protocol :+: eff) '(p, sr) '(End, sr) b ->
-  Sem eff sr sr (a, b)
-connect a b = undefined
+-- class Connect p1 where
+--   type Succ p1
+--   connect :: (q1 ~ Dual p1, q1 ~ Dual p1) => -- , Dual (Succ p1) ~ q2, Dual q2 ~ Succ p1) =>
+--     Sem (Protocol :+: eff1) '(p1, sr1) '(Succ p1, sr1) a ->
+--     Sem (Protocol :+: eff2) '(q1, sr2) '(q2, sr2) b ->
+--     (Sem eff1 sr1 sr1 a, Sem eff2 sr2 sr2 b)
+
+-- instance Connect End End where
+--   connect (Value x) (Value y) = Ix.return (x, y)
+--   connect (Op (Inr op1) k1) (Op (Inr op2) k2) = connect (Op op1 undefined) (Op op2 undefined)
+--   connect sl@(Value _) (Op (Inr op2) k2) =
+
+-- instance Connect p => Connect (a :! p) where
+--   type Succ (a :! p) = p
+--   -- type Succ2 (a :=)
+
+--   connect (Op (Inl (Send (a :: a))) k1) (Op (Inl Recv) k2) =
+--     connect (runIKleisliTupled k1 ()) (runIKleisliTupled k2 a)
+--   -- connect sl@(Op (Inl _) _) (Op (Inr op) k2) = Op op $ \x -> connect sl (runIKleisliTupled k2 x)
+--   connect _ _ = undefined
+
+connect :: Dual p1 ~ q1 =>
+  Sem (Protocol :+: IIdentity) '(p1, ()) '(End, ()) a ->
+  Sem (Protocol :+: IIdentity) '(q1, ()) '(End, ()) b ->
+  (a, b)
+connect (Value x) (Value y) = (x, y)
+connect (Op (Inl Recv) k1) (Op (Inl (Send a)) k2) = connect (runIKleisliTupled k1 a) (runIKleisliTupled k2 ())
+connect (Op (Inl (Send a)) k1) (Op (Inl Recv) k2) = connect (runIKleisliTupled k1 ()) (runIKleisliTupled k2 a)
+
+
