a338d5a23992f77c2885fba83c7a5e29bcee6c24
Fendor <power.walross@gmail.com>
2022-12-15 11:44:46 +0100

    Don't try to implement weaving for Protocol yet

--- src/Simple/Examples.hs
+++ src/Simple/Examples.hs
@@ -16,30 +16,30 @@ readerExample = Ix.do
   x <- ask
   Ix.return x
 
-runStateProt ::
-  e ->
-  MiniEff (StateS e: effs) Protocol p q a ->
-  MiniEff effs Protocol p q (e, a)
-runStateProt s (Value a) = Ix.return (s, a)
-runStateProt s (Impure (OHere GetS) k) = runStateProt s (runIKleisliTupled k s)
-runStateProt _s (Impure (OHere (PutS s')) k) = runStateProt s' (runIKleisliTupled k ())
-runStateProt s (Impure (OThere cmd) k) = Impure cmd $ IKleisliTupled (runStateProt s . runIKleisliTupled k)
-runStateProt s (ImpureP cmd k) = ImpureP cmd (IKleisliTupled $ runStateProt s . runIKleisliTupled k)
-runStateProt s (ScopedP op k) = case op of
-  LoopCUnbounded m -> Ix.do
-    aop <- runS s m
-    ScopedP aop
-      (IKleisliTupled $ \(e, x) -> runStateProt e $ runIKleisliTupled k x)
+-- runStateProt ::
+--   e ->
+--   MiniEff (StateS e: effs) Protocol p q a ->
+--   MiniEff effs Protocol p q (e, a)
+-- runStateProt s (Value a) = Ix.return (s, a)
+-- runStateProt s (Impure (OHere GetS) k) = runStateProt s (runIKleisliTupled k s)
+-- runStateProt _s (Impure (OHere (PutS s')) k) = runStateProt s' (runIKleisliTupled k ())
+-- runStateProt s (Impure (OThere cmd) k) = Impure cmd $ IKleisliTupled (runStateProt s . runIKleisliTupled k)
+-- runStateProt s (ImpureP cmd k) = ImpureP cmd (IKleisliTupled $ runStateProt s . runIKleisliTupled k)
+-- runStateProt s (ScopedP op k) = case op of
+--   LoopCUnbounded m -> Ix.do
+--     aop <- runS s m
+--     ScopedP aop
+--       (IKleisliTupled $ \(e, x) -> runStateProt e $ runIKleisliTupled k x)
 
-  _ -> undefined
+--   _ -> undefined
 
-runS :: e
-  -> MiniEff (StateS e: effs) Protocol p' q' x
-  -> MiniEff effs Protocol p' q'
-      (ScopedE Protocol (MiniEff effs Protocol) p p' q' q (e, x) (e, [x]))
-runS e act = Ix.do
-  let m' = runStateProt e act
-  Ix.return $ LoopCUnbounded m'
+-- runS :: e
+--   -> MiniEff (StateS e: effs) Protocol p' q' x
+--   -> MiniEff effs Protocol p' q'
+--       (ScopedE Protocol (MiniEff effs Protocol) p p' q' q (e, x) (e, [x]))
+-- runS e act = Ix.do
+--   let m' = runStateProt e act
+--   Ix.return $ LoopCUnbounded m'
   -- LoopSUnbounded m ->
   --   let
   --     n = nt (m <$ ctx)
