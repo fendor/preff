bcb3872c97a44fa008de99d7263b06fc3c48d665
Fendor <power.walross@gmail.com>
2023-01-05 12:12:42 +0100

    Fix store a bit

--- src/Store.hs
+++ src/Store.hs
@@ -1,18 +1,18 @@
 {-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE RebindableSyntax #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# OPTIONS_GHC -fplugin=Plugin #-}
 
 module Store where
 
-import Parameterised.State
+import MiniEff.Parameterised.State
 import MiniEff
 import Prelude hiding (Monad (..), (=<<))
+import Control.IxMonad as Ix
 
 newtype Task = Task ()
 newtype Machine = Machine ()
 
-run :: Task -> Machine -> MiniEff StateF StateG p p ()
+run :: Task -> Machine -> MiniEff effs StateF p p ()
 run = undefined
 
 machineInit :: Machine
@@ -25,8 +25,8 @@ task2 = undefined
 (=<<) :: (IMonad m) => ((a -> m j k b) -> m i j a -> m i k b)
 (=<<) = flip (>>=)
 
-lazyInit :: ( 'R ≤ Lookup j n, 'X ≤ Lookup j n) => Token (Maybe Machine) n -> MiniEff StateF g j j Machine
-lazyInit cache = do
+lazyInit :: ( 'R ≤ Lookup j n, 'X ≤ Lookup j n) => Token (Maybe Machine) n -> MiniEff effs StateF j j Machine
+lazyInit cache = Ix.do
   get cache >>= \case
     Just machine -> do
       --demandPut cache
@@ -36,10 +36,10 @@ lazyInit cache = do
       put cache (Just machine)
       return machine
 
-runMultipleTasks :: MiniEff StateF StateG i k ()
+runMultipleTasks :: MiniEff effs StateF i k ()
 runMultipleTasks = do
   cache <- alloc Nothing
-  _ <- fork (run task1 =<< lazyInit cache)
+  _ <- fork (Store.run task1 =<< lazyInit cache)
   -- fork (run task2 =<< lazyInit cache)
   return ()
 
@@ -56,14 +56,14 @@ transfer ::
   Token a n1 ->
   Token a n2 ->
   a ->
-  MiniEff StateF g k k ()
+  MiniEff effs StateF k k ()
 transfer sender recipient amount = do
   v_sender <- get sender
   v_recipient <- get recipient
   put sender (v_sender - amount)
   put recipient (v_recipient + amount)
 
-racyBank :: MiniEff StateF StateG i k (Future ())
+racyBank :: MiniEff effs StateF i k (Future ())
 racyBank = do
   alice <- alloc (pounds 10)
   bob <- alloc (pounds 10)
@@ -71,7 +71,7 @@ racyBank = do
 
 --fork (transfer alice bob (pounds 5))
 
-test :: MiniEff StateF StateG i k ()
+test :: MiniEff effs StateF i k ()
 test = do
   a <- alloc False
   put a True
