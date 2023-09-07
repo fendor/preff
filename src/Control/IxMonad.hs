2784e0a82fc15b6256054de5887f08b0b3de13cd
Fendor <power.walross@gmail.com>
2023-09-07 12:01:17 +0200

    More iMonad utils

--- src/Control/IxMonad.hs
+++ src/Control/IxMonad.hs
@@ -1,28 +1,38 @@
 {-# LANGUAGE UndecidableInstances #-}
+
 module Control.IxMonad where
 
-import Prelude hiding (Monad(..), Applicative(..), Functor(..), mapM)
 import Data.Kind
+import Prelude hiding (Applicative (..), Functor (..), Monad (..), mapM, (=<<))
 
 -- ------------------------------------------------
--- Parametric Effect monad
+-- Parametric indexed monad
 -- ------------------------------------------------
 
 type IMonad :: (p -> p -> Type -> Type) -> Constraint
-class IApplicative m => IMonad m where
+class (IApplicative m) => IMonad m where
   return :: a -> m i i a
   (>>=) :: m i j a -> (a -> m j k b) -> m i k b
   (>>) :: m i j a -> m j k b -> m i k b
   g >> f = g >>= const f
+
+(=<<) :: (IMonad m) => (a -> m j k b) -> m i j a -> m i k b
+f =<< m = m >>= f
+
+(>=>) :: (IMonad m) => (a -> m i j b) -> (b -> m j k c) -> (a -> m i k c)
+f >=> g = \a -> (f a >>= g)
+
+(<=<) :: (IMonad m) => (b -> m j k c) -> (a -> m i j b) -> (a -> m i k c)
+f <=< g = \a -> (f =<< g a)
 
 type IFunctor :: (p -> p -> Type -> Type) -> Constraint
 class IFunctor f where
   imap :: (a -> b) -> f p q a -> f p q b
 
   (<$) :: a -> f p q b -> f p q a
-  (<$) =  imap . const
+  (<$) = imap . const
 
-class IFunctor f => IApplicative f where
+class (IFunctor f) => IApplicative f where
   pure :: a -> f i i a
   (<*>) :: f i j (a -> b) -> f j r a -> f i r b
 
@@ -49,33 +59,43 @@ foldM [x] c f =
 foldM (x : xs) c f =
   f x c >>= \c' -> foldM xs c' f
 
-replicateM_ :: IApplicative f => Int -> f p p a -> f p p ()
+replicateM_ :: (IApplicative f) => Int -> f p p a -> f p p ()
 replicateM_ cnt0 f =
-    loop cnt0
-  where
-    loop cnt
-        | cnt <= 0  = pure ()
-        | otherwise = f *> loop (cnt - 1)
+  loop cnt0
+ where
+  loop cnt
+    | cnt <= 0 = pure ()
+    | otherwise = f *> loop (cnt - 1)
 
-replicateM :: IApplicative f => Int -> f p p a -> f p p [a]
+replicateM :: (IApplicative f) => Int -> f p p a -> f p p [a]
 replicateM cnt0 f =
-    loop cnt0
-  where
-    loop cnt
-        | cnt <= 0  = pure []
-        | otherwise = (imap (\x -> \xs -> (x:xs)) f) <*> loop (cnt - 1)
+  loop cnt0
+ where
+  loop cnt
+    | cnt <= 0 = pure []
+    | otherwise = (imap (\x -> \xs -> (x : xs)) f) <*> loop (cnt - 1)
+
+forM :: (IMonad m) => [a] -> (a -> m i i b) -> m i i [b]
+forM [] _ = pure []
+forM [x] f = Control.IxMonad.do
+  c <- f x
+  pure [c]
+forM (x : xs) f = Control.IxMonad.do
+  c <- f x
+  cs <- forM xs f
+  pure (c : cs)
 
 forM_ :: (IMonad m) => [a] -> (a -> m i i ()) -> m i i ()
-forM_ [] _ = return ()
-forM_ [x] f =
-  f x
-forM_ (x : xs) f =
-  f x >>= \_c -> forM_ xs f
+forM_ xs f = Control.IxMonad.do
+  _ <- forM xs f
+  pure ()
 
-mapM :: IMonad m => (a -> m p p b) -> [a] -> m p p [b]
-mapM f [] = return []
-mapM f (x:xs) =
-  f x >>= \c -> imap (c:) $ mapM f xs
+mapM :: (IMonad m) => (a -> m p p b) -> [a] -> m p p [b]
+mapM _ [] = return []
+mapM f (x : xs) =
+  f x >>= \c -> imap (c :) $ mapM f xs
 
--- sequenceA :: (Traversable t, IApplicative f) => t (f p p a) -> f p p (t a)
--- sequenceA =
+mapM_ :: (IMonad m) => (a -> m p p b) -> [a] -> m p p ()
+mapM_ f xs = Control.IxMonad.do
+  _ <- mapM f xs
+  pure ()
